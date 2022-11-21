15950f220a80a0b7021a5bb11d349d7adb865c17
Karandeep0280 <109850363+Karandeep0280@users.noreply.github.com>
2022-11-21 18:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.2.0"
-    }
-  }
-}
-
-resource "aws_instance" "punch" {
-    ami  ="ami-09de362f44ba0a166"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "karan"
-      Owner= "karandeep"
-  }
-}
-
-provider "aws"{
-  region ="ap-south-1"
+resource "aws_instance" "karan" {
+    instance_type = var.instance_type
+    ami = var.ami2
+    tags = var.tags
 }
