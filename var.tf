45dd8706ef08db0cf36b0af1728b91fc2b38a0f6
Karandeep0280 <109850363+Karandeep0280@users.noreply.github.com>
2022-11-21 18:24:48 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,33 +1,14 @@
-variable "ami" {
-    type = list(string)
-    description = "assign the ami to server"
+variable "instance_type" {
+  type = string
   
 }
 
-
-variable "name" {
-    type = list(string)
-    description =  "assign the name to the server"
-}
-variable "jak" {
-    type = list(string)
-    description = "assign the name  to owner"
+variable "ami2" {
+type = string
   
 }
 
+variable "tags" {
+   type = map(any)
   
-  variable "karan10" {
-    type  = map(any)
-    description = "pass the value"
-    
-  }
-  variable "chandigarh" {
-    type = set(string)
-    description = "pass the value"
-    
-  }
-  variable "buckname" {
-    description = "pass the name of bucket"
-    type = map(any)
-    
-  }
+}
